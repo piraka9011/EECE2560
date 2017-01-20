5774f9d391925cc06f0f9370acbaf5da4a570816
BlackEagleSA <alrifaie.t@husky.neu.edu>
2017-01-19 19:50:08 -0500

    Proto_MainFunction

--- Proj1a/src/main.cpp
+++ Proj1a/src/main.cpp
@@ -1,7 +1,36 @@
-//TEST COMMENT BY TURKI 01/16
+
 #include <iostream>
+#include <vector>
+#include "../include/code.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //std::cout << "Hello, World!" << std::endl;
+
+    //Declaring the length of the code and the range of the numbers variables
+    int m,n;
+
+    //Prompting the user for two inputs to assign to length and range
+    std::cout << "Please enter the length of the desired secret code followed by the range of numbers\n";
+    std::cin >> n >> m;
+
+    //Creating two objects in class code; Where CG (Code Guess) and SC(Secret Code).
+    code SC(n,m);
+    code GC(n,m);
+    SC.randomInit();
+    //counter for the while loop that breaks if the user failed to win in the first 10 times (Loses).
+    int count=0;
+
+    //Reminder of the parameters that the user passed
+    std::cout<<"Enter your guess\n Remember that the code length is "
+             <<n<<" and the range is from "<<0 << " To " <<m << std::endl;
+
+    //While loop to implement the calss functions
+    while(count!=10) {
+        //Calling the check correct class function that also prompts the user for inputs
+        SC.checkCorrect(GC);
+
+    }
+
     return 0;
 }
