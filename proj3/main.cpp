6f47f524a6a15b0a722c15679b0ce48425cfd42b
BlackEagleSA <alrifaie.t@husky.neu.edu>
2017-03-11 21:20:27 -0500

    Comparison with dictionary to be added

--- proj3/main.cpp
+++ proj3/main.cpp
@@ -8,26 +8,46 @@ void findMatches(Dictionary dict, Grid mat)
     std::vector<std::string> possibleWords;
     std::string tempWord;
     matrix<char> grid = mat.getMat();
+    int x[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int y[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
     // Grid Starting points loop
     for (int posX = 0; posX < grid.rows(); posX++)
     {
         for (int posY = 0; posY < grid.cols(); posY++)
         {
-            // Start traversing for words around starting points
-            for (int i = 2; i < grid.rows(); i++)
-            {
-                for (int j = 2; j < grid.cols(); j++)
+            //Intis the first letter to search around
+            tempWord = grid[posX].at(posY);
+
+            //move in all 8 directions
+            for(int direction=0; direction < 8 ; direction++){
+                int k, rowD = posX + x[direction], colD = posY + y[direction];
+
+                while (rowD != posX || colD != posY)
                 {
-                    tempWord = tempWord + grid[posX].at(posY) +
-                               grid[posX + 1].at(posY + 1);
+
+                    //Wrap logic:
+                    // column wraps
+                    if (colD == grid.cols())
+                        colD = 0;
+                    if (colD ==-1)
+                        colD = grid.cols()-1;
+                    //row wraps
+                    if (rowD == grid.rows())
+                        rowD = 0;
+                    if (rowD ==-1)
+                        rowD = grid.rows();
+
+                    tempWord = tempWord + grid[rowD].at(colD);
+                    rowD =+ x[direction], colD= y[direction];
                 }
+                possibleWords.push_back(tempWord);
+            }
 
             }
 
         }
 
     }
-}
 
 void search()
 {
