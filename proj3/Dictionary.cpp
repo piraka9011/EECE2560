9855a212ac3090c1614b4a78cf553fb2830dc7f1
piraka9011 <piraka9011@hotmail.com>
2017-03-13 21:49:07 -0400

    Final working proj

--- proj3/Dictionary.cpp
+++ proj3/Dictionary.cpp
@@ -1,22 +1,50 @@
-//
-// Created by piraka9011 on 2/27/17.
-//
+/**
+	EECE2560: Proj3a
+	Dictionary.cpp
+	Purpose: This file provides the implementation for the Dictionary class.
+    It allows for reading in the dictionary words to a vector, sorting them
+    using the very very efficient selection sort,and then searching the
+    dictionary for a word using the very very inefficient binary search.
+	@author: Anas Abou Allaban & Turki Alrifaie
+	@version: 1.0 3/13/17
+*/
 
 #include "Dictionary.h"
 
+/**
+    A constructor that specifies the directory in which the sorted dictionary
+    will be saved.The saved file will be used if it was located, otherwise it
+    will declare the file directory of the unsorted dictionary.
+                                **** NOTE TO TA ****
+    Please note that this program was tested on Ubuntu 14.04. Other students
+    reported errors with opening the file the way we do. Please make sure that
+    you put in the correct file path extension.
+    If the file is in the same directory as the project, you may not need to
+    add the file path extension.
+    ***************************************************************************
+ */
 Dictionary::Dictionary()
 {
-    // Ubuntu Path /home/piraka9011/Desktop/algo_ws/proj3/
+    /// CHANGE DIRECTORY HERE
     std::ifstream savedFile("/home/piraka9011/Desktop/algo_ws/proj3/sortedDict.txt");
+    // Check to see if the dictionary was previously sorted
     if (savedFile.is_open()) {
+        // If it is, set the directory accordingly and inform user
         savedDict = true;
+        /// CHANGE DIRECTORY HERE
         fileDir = "/home/piraka9011/Desktop/algo_ws/proj3/sortedDict.txt";
         std::cout << "Using previously sorted dictionary\n";
     }
+    // Otherwise use default dictionary
     else
+        /// CHANGE DIRECTORY HERE
         fileDir = "/home/piraka9011/Desktop/algo_ws/proj3/dictionary.txt";
 }
 
+/**
+    This function opens the file dictionary and stores all of the dictionary
+    text file contents into a vector.
+  */
 void Dictionary::readDictionary()
 {
     // Temp string var
@@ -37,10 +65,14 @@ void Dictionary::readDictionary()
         // Close the istream
         inFile.close();
     }
-    else
+    else // Uh-oh x2
         std::cout << "ERROR: UNABLE TO OPEN FILE\n";
 }
 
+/**
+    This function sorts the contents of the dictionary using selection sort method.
+    The function also saves a sorted copy of the dictionary if it is not saved.
+ */
 void Dictionary::selectionSort()
 {
     int min = 0;
@@ -64,7 +96,8 @@ void Dictionary::selectionSort()
             std::swap(stringDict.at(i), stringDict.at(min));
     }
     // Save dictionary after sorting
-    saveDict();
+    if (!savedDict)
+        saveDict();
 }
 
 void Dictionary::saveDict()
@@ -77,6 +110,13 @@ void Dictionary::saveDict()
     outFile.close();
 }
 
+/**
+    This function uses the binary search algorithm to search for a word
+    in the sorted dictionary.
+    @param string word: The key word to search for
+    @return int middle: When the word is found, middle is the index where the
+            word is located in the sorted dictionary
+ */
 int Dictionary::searchWord(std::string word)
 {
     int first = 0, last = stringDict.size() - 1, middle;
@@ -86,25 +126,33 @@ int Dictionary::searchWord(std::string word)
     {
         // Set our middle search position
         middle = (first + last) / 2;
-        // If we found the word, return it
+        // If its greater, set the range to the 'right' side.
         if (stringDict.at(middle) < word)
             first = middle + 1;
         // If the word in the middle is greater than our key word,
         // set the range to be the 'left' side.
         else if (stringDict.at(middle) > word)
             last = middle - 1;
-        // If its greater, set the range to the 'right' side.
+        // Any other case means we found our word, return position
         else
         {
             std::cout << "found: " << word << '\n';
             return middle;
         }
     }
-    return 0;
+    // If no word is found, return -1
+    return -1;
 }
 
+/**
+    An overloaded print out operator that prints out the entire dictionary.
+    @param  ostream& os: Outputstream object
+            Dictionary d: Our dictionary object that we will read words from
+    @return osrteam& os: Output stream object to output to console.
+ */
 std::ostream& operator<< (std::ostream &os, Dictionary d)
 {
     for (int i = 0; i < d.getDict().size(); i++)
         os << d.getDict().at(i) << '\n';
+    return os;
 }
--- proj3/Grid.cpp
+++ proj3/Grid.cpp
@@ -1,13 +1,30 @@
-//
-// Created by piraka9011 on 3/1/17.
-//
+/**
+	EECE2560: Proj3a
+	Grid.cpp
+	Purpose: This file provides the implementation for the Grid class. Its only
+    implementation is that of the constructor as that is all that is needed.
+	@author: Anas Abou Allaban & Turki Alrifaie
+	@version: 1.0 3/13/17
+*/
 
 #include "Grid.h"
 
+/**
+    The Grid constructor opens the grid file, reads in the rows/cols, resizes
+    the grid accordingly and then stores each character into the grid
+    @param  string matrixFile: The name of the file to be opened
+                                **** NOTE TO TA ****
+    Please note that this program was tested on Ubuntu 14.04. Other students
+    reported errors with opening the file the way we do. Please make sure that
+    you put in the correct file path extension.
+    If the file is in the same directory as the project, you may not need to
+    add the file path extension.
+    ***************************************************************************
+ */
 Grid::Grid(std::string matrixFile)
 {
     // Add directory to file
-    // Ubuntu path /home/piraka9011/Desktop/algo_ws/proj3/
+    /// CHANGE DIRECTORY HERE
     std::string fileName = "/home/piraka9011/Desktop/algo_ws/proj3/" + matrixFile;
     // Open file
     std::ifstream inFile (fileName.c_str());
